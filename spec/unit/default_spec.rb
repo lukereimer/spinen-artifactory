10359f1010532702732e7473890aa97e56b8b278
Keli <keli.grubb@spinen.com>
2015-03-11 10:34:22 -0400

    made changes to the default_spec to make it run the tests.

---
+++ spec/unit/default_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'spinen-artifactory::default' do
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new do |node|
+      node.set['artifactory']['home'] = '/var/log/artifactory'
+    end.converge(described_recipe)
+  end
+
+  it 'includes apt::default recipe' do
+    expect(chef_run).to include_recipe('java::default')
+  end
+
+  it 'creates an artifactory user' do
+    expect(chef_run).to create_user(node['artifactory']['user'])
+  end
+
+  %w(
+    node['artifactory']['home']
+    node['artifactory'][catalina_base]
+    ::File.join(node['artifactory']['catalina_base'], 'work')
+    ::File.join(node['artifactory']['catalina_base'], 'temp')
+    node['artifactory']['log_dir']
+  ).each do |delta|
+    it 'does stuff' do
+      expect(chef_run).to create_directory(delta).with(
+        owner: node['artifactory']['user'],
+        mode: 0755,
+        recursive: true
+      )
+    end
+  end
+end
